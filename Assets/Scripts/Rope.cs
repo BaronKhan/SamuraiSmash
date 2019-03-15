0f105e4a8da1a6443fb912ab0e3f33d5decb8293
BaronKhan <bksonic@hotmail.co.uk>
2019-03-15 19:54:55 +0000

    Update headbnad physics

--- Assets/Scripts/Rope.cs
+++ Assets/Scripts/Rope.cs
@@ -19,6 +19,7 @@ public class Rope : MonoBehaviour
     for (int i = 0; i < links; ++i)
     {
       GameObject link = Instantiate(linkPrefab, transform);
+      link.transform.position = new Vector3(link.transform.position.x, link.transform.position.y - 0.1F, link.transform.position.z);
       HingeJoint joint = link.GetComponent<HingeJoint>();
       joint.connectedBody = previousRB;
       previousRB = link.GetComponent<Rigidbody>();
