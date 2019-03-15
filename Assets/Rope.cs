11bd511af9ab8249e90c3c8a88653f02c6629340
BaronKhan <bksonic@hotmail.co.uk>
2019-03-15 18:56:44 +0000

    Add basic rope physics

---
+++ Assets/Rope.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+  public Rigidbody hook;
+
+  public GameObject linkPrefab;
+
+  public int links = 14;
+  
+  void Start()
+  {
+    GenerateRope();
+  }
+
+  void GenerateRope()
+  {
+    Rigidbody previousRB = hook;
+    for (int i = 0; i < links; ++i)
+    {
+      GameObject link = Instantiate(linkPrefab, transform);
+      HingeJoint joint = link.GetComponent<HingeJoint>();
+      joint.connectedBody = previousRB;
+      previousRB = link.GetComponent<Rigidbody>();
+    }
+  }
+
+
+}
