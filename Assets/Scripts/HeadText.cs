8aa34bfca27f1af1fe0c06f5605077de79d5307e
BaronKhan <bksonic@hotmail.co.uk>
2020-10-18 19:58:42 +0100

    Add custom head text

---
+++ Assets/Scripts/HeadText.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadText : MonoBehaviour
+{
+  private enum TextType
+  {
+    Int,
+    Float,
+    Fraction,
+    Symbol
+  };
+
+  public TextMesh text_mesh_frac = null;
+
+  private TextMesh text_mesh = null;
+  private TextType text_type = TextType.Int;
+  private readonly int text_max_len = 5;
+  private readonly int text_frac_max_len = 3;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    text_mesh = GetComponent<TextMesh>();
+    if (!text_mesh_frac)
+    {
+      Debug.LogError("Destroying Enemy as TextMeshFrac not found");
+      Destroy(gameObject);
+      return;
+    }
+
+    SetInteger(1);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+        
+  }
+
+  private bool SetText(string s)
+  {
+    int len = text_mesh.text.Length;
+    if (len > text_max_len)
+    {
+      Debug.LogWarning($"Input \"{s}\" is greater than {text_max_len} ({len})");
+      return false;
+    }
+
+    text_mesh.text = s;
+    text_mesh_frac.text = "";
+    text_mesh.characterSize = Mathf.Max(1.0f, 2.0f - (0.25f * (len - 1)));
+
+    return true;
+  }
+
+  public bool SetInteger(int i)
+  {
+    text_type = TextType.Int;
+    return SetText(i.ToString());
+  }
+
+  public bool SetFloat(float f)
+  {
+    text_type = TextType.Float;
+    return SetText(f.ToString());
+  }
+  public bool SetSymbol(char c)
+  {
+    text_type = TextType.Symbol;
+    return SetText(c.ToString());
+  }
+  public bool SetFraction(int num, int den)
+  {
+    text_type = TextType.Fraction;
+    string num_s = num.ToString();
+    string den_s = den.ToString();
+    int frac_length = Math.Max(num_s.Length, den_s.Length);
+    if (frac_length > text_frac_max_len)
+    {
+      Debug.LogWarning($"Fraction length \"{num_s}/{den_s}\" is greater than {text_frac_max_len} ({frac_length})");
+      return false;
+    }
+
+    text_mesh.text = $"{num_s}\n{den_s}";
+    text_mesh_frac.text = "";
+    for (int i = 0; i <= frac_length; ++i) { text_mesh_frac.text += '_'; }
+    text_mesh.characterSize = 1.0f;
+
+    return true;
+  }
+}
