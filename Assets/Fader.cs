ba67374471a13ea73a1c3ea9979405b1117c3437
BaronKhan <bksonic@hotmail.co.uk>
2020-12-20 00:39:40 +0000

    Add Fader

---
+++ Assets/Fader.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Fader : MonoBehaviour
+{
+  public bool fade_in = false;
+  public float m_fade_speed = 1f;
+
+  private Image m_image = null;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    if (fade_in)
+      SetFadeType(false);
+  }
+
+  IEnumerator FadeIn()
+  {
+    for (float ft = 1f; ft >= 0; ft -= 0.16f * m_fade_speed)
+    {
+      Color c = m_image.color;
+      c.a = ft;
+      m_image.color = c;
+      yield return null;
+    }
+
+    Destroy(gameObject);
+  }
+  IEnumerator FadeOut()
+  {
+    for (float ft = 0f; ft < 1; ft += 0.16f * m_fade_speed)
+    {
+      Color c = m_image.color;
+      c.a = ft;
+      m_image.color = c;
+      yield return null;
+    }
+
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  public Fader SetFadeType(bool fade_out)
+  {
+    return SetFadeType(fade_out, m_fade_speed);
+  }
+
+  public Fader SetFadeType(bool fade_out, float fade_speed)
+  {
+    m_fade_speed = fade_speed;
+    m_image = GetComponent<Image>();
+    m_image.color = new Color(0, 0, 0, fade_out ? 0 : 1);
+
+    StartCoroutine(fade_out ? FadeOut() : FadeIn());
+    return this;
+  }
+}
--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -6,11 +6,12 @@ using UnityEngine.SceneManagement;
 public class Title : MonoBehaviour
 {
   private bool started = false;
+  private Fader fader = null;
 
   // Start is called before the first frame update
   void Start()
   {
-
+    fader = GetComponentInChildren<Fader>();
   }
 
   // Update is called once per frame
@@ -19,7 +20,8 @@ public class Title : MonoBehaviour
     if (!started && Input.GetMouseButtonDown(0))
     {
       // TODO: fade in and out
-      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+      /*SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);*/
+      fader.SetFadeType(true, 0.25f);
       started = true;
     }
   }
